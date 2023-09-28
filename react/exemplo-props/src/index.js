e019f33f51ffccbf0eaa60c35da3b86ba41e0304
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-28 19:44:37 -0300

    ap04(props): codigo 2.2.1

---
+++ react/exemplo-props/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return <div>Um componente</div>
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
